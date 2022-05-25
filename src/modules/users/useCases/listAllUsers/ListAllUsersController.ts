f2a2beb6df583237e6af7826512f82f910ea3d1a
Gabriel Silva <gabriel.nascimento@koin.com.br>
2022-05-25 08:22:49 -0300

    finish application

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    try {
+      const result = this.listAllUsersUseCase.execute({
+        user_id: user_id as string,
+      });
+      return response.json(result);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
